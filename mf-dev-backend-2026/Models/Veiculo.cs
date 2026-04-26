84fcd44b246358a92325e838b54d1b01f03ec226
yasmini98 <oliveira.yasmini@gmail.com>
2026-04-26 14:13:45 -0300

    Configuração do Banco de Dados e da Tabela Veículo

---
+++ mf-dev-backend-2026/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2026.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do veículo é obrigatório.")]
+        public string NomeVeiculo { get; set; }
+
+        [Required(ErrorMessage = "A placa veículo é obrigatório.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "O ano de fabricação do veículo é obrigatório.")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "O ano do modelo do veículo é obrigatório.")]
+        public int AnoModelo { get; set; }
+    }
+}
